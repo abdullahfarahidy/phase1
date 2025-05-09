72cd0971f3d5ad33f2dfda7b04be7899299b0c5c
abdulla_En <abdulla4112018@gmail.com>
2025-05-09 23:02:16 +0300

    Refactor and Improve JwtAuthenticationFilter.java [closes 43]

--- lms-backend/src/main/java/net/java/lms_backend/Security/Jwt/JwtAuthenticationFilter.java
+++ lms-backend/src/main/java/net/java/lms_backend/Security/Jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,6 @@
-    package net.java.lms_backend.Security.Jwt;
+    package net.java.lms_backend.security.jwt;
 
-    import io.jsonwebtoken.Claims;
+    
     import jakarta.servlet.FilterChain;
     import jakarta.servlet.http.HttpServletRequest;
     import jakarta.servlet.http.HttpServletResponse;
@@ -10,14 +10,14 @@
     import org.springframework.web.filter.OncePerRequestFilter;
     import jakarta.servlet.ServletException;
     import java.io.IOException;
-    import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 
     import java.util.ArrayList;
-    import java.util.List;
+
 
     public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-        private AuthenticationManager authenticationManager;
+
         private JwtUtil jwtUtil;
 
         public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
